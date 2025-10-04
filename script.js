d3dc1efd10c45ad834fcc1a4196230fd01c0a952
swapnil404 <swapnil.christian.developer@gmail.com>
2025-10-05 00:22:49 +0530

    money script

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let money = 1000;
 
+function updateMoneyDisplay() {
+  document.getElementById("money").textContent = `Money: $${money}`;
+}
+
+function addMoney(amount) {
+  money += amount;
+  updateMoneyDisplay();
+}
+
+function subtractMoney(amount) {
+  if (money >= amount) {
+    money -= amount;
+    updateMoneyDisplay();
+    return true;
+  }
+  return false; 
+}
