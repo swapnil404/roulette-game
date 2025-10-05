6aa0dbde3b73c864bc5c05ddd869a88cd229eb8b
swapnil404 <swapnil.christian.developer@gmail.com>
2025-10-06 02:00:51 +0530

    wheel

--- script.js
+++ script.js
@@ -42,10 +42,10 @@ function black() {
   updateColourDisplayBlack();
   colour = "black";
   lost = "red";
-  ``;
 }
 
 function spin() {
+  document.getElementById("result").innerHTML = "";
   amount = document.getElementById("bet-amount");
   if (amount.value == "") {
     alert("Enter Bet Below To Spin!");
@@ -60,12 +60,44 @@ function spin() {
     let remainder = number % 2;
     if (remainder == 0) {
       display_color = colour;
-      alert("YOU WON $" + amount.value);
-      addMoney(amount.value);
+      wheel();
+      const myTimeout1 = setTimeout(() => {
+        document.getElementById("result").innerHTML =
+          "YOU WON $" + amount.value;
+        addMoney(amount.value);
+        // document.getElementById("spin-circle").style.animation = "none";
+        // document.getElementById("spin-circle").style.rotate = "0deg";
+        document.getElementById("result").style.animation =
+          "winColorChange 1s infinite";
+      }, 2000);
     } else {
       display_color = lost;
-      alert("You Lost $" + amount.value);
-      subtractMoney(amount.value);
+      wheel();
+      const myTimeout2 = setTimeout(() => {
+        document.getElementById("result").innerHTML =
+          "YOU Lost $" + amount.value;
+
+        subtractMoney(amount.value);
+        // document.getElementById("spin-circle").style.animation = "none";
+        // document.getElementById("spin-circle").style.rotate = "0deg";
+        document.getElementById("result").style.animation =
+          "colorChange 1s infinite";
+      }, 2000);
     }
+  }
+}
+
+function wheel() {
+  console.log(display_color);
+  if (display_color == "red") {
+    document.getElementById("spin-circle").style.animation =
+      "spin_red 2s forwards";
+    // document.getElementById("spin-circle").style.rotate = "0deg";
+    // document.getElementById("spin-circle").style.rotate = "180deg";
+  } else {
+    document.getElementById("spin-circle").style.animation =
+      "spin_black 2s forwards";
+    // document.getElementById("spin-circle").style.rotate = "0deg";
+    // document.getElementById("spin-circle").style.rotate = "90deg";
   }
 }
